bab42e8c758e1f087d334e2a2eed14179bb52f22
Leonardo Henrique Vieira <leohvir@gmail.com>
2020-07-15 02:31:13 -0300

    Adding style to components

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,7 +1,60 @@
 import React from 'react';
+import { FiChevronRight } from 'react-icons/fi';
+
+import logoImg from '../../assets/logo.svg';
+
+import { Title, Form, Repositories } from './styles';
 
 const Dashboard: React.FC = () => {
-  return <h1>Dashboard</h1>;
+  return (
+    <>
+      <img src={logoImg} alt="Github Explorer" />
+      <Title>Explore repositórios no github</Title>
+
+      <Form action="">
+        <input placeholder="Digite o nome do repositório" />
+        <button type="submit">Pesquisar</button>
+      </Form>
+
+      <Repositories>
+        <a href="#">
+          <img
+            src="https://avatars0.githubusercontent.com/u/53581258?s=460&u=2e1869f145581b571cdd167edd5fa0f50aca961b&v=4"
+            alt="Profile img"
+          />
+          <div>
+            <strong>leonarhv/Agenda-contatos</strong>
+            <p>Projeto Agenda de contatos usando SPRING + REACT</p>
+          </div>
+          <FiChevronRight />
+        </a>
+
+        <a href="#">
+          <img
+            src="https://avatars0.githubusercontent.com/u/53581258?s=460&u=2e1869f145581b571cdd167edd5fa0f50aca961b&v=4"
+            alt="Profile img"
+          />
+          <div>
+            <strong>leonarhv/Agenda-contatos</strong>
+            <p>Projeto Agenda de contatos usando SPRING + REACT</p>
+          </div>
+          <FiChevronRight />
+        </a>
+
+        <a href="#">
+          <img
+            src="https://avatars0.githubusercontent.com/u/53581258?s=460&u=2e1869f145581b571cdd167edd5fa0f50aca961b&v=4"
+            alt="Profile img"
+          />
+          <div>
+            <strong>leonarhv/Agenda-contatos</strong>
+            <p>Projeto Agenda de contatos usando SPRING + REACT</p>
+          </div>
+          <FiChevronRight />
+        </a>
+      </Repositories>
+    </>
+  );
 };
 
 export default Dashboard;
